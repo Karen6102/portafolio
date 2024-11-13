5d60aba7d1744ed81d0ed2773052ad26963d2625
Karen6102 <ana.lopez.ji@usb.edu.mx>
2024-11-13 09:21:13 -0600

    Add files via upload

---
+++ primeros pasos.py
@@ -0,0 +1,33 @@
+# imprimir en pantalla
+print("Hola Mundo")
+
+#suma 1.1
+#pedimos al usuario que ingrese los dos números
+numero1 = float(input("ingrese el primer número:"))
+numero2 = float(input("ingrese el segundo número:"))
+
+# sumamos los números
+suma = numero1 + numero2
+
+#imprimimos el resultado
+print("la suma de", numero1, "y",numero2,"es:",suma)
+
+#suma 1.1
+#resta 1.1
+#multiplicación 1.1
+#división 1.1
+#pedimos al usuario que ingrese los dos números
+numero1 = float(input("ingrese el primer número:"))
+numero2 = float(input("ingrese el segundo número:"))
+
+#Sumamos,multiplicamos,restamos,divimos los números
+suma = numero1 + numero2 
+resta = numero1 - numero2
+multiplicación = numero1 * numero2
+división = numero1/numero2
+
+#imprimimos el resultado
+print("la suma de ",numero1,"y",numero2,"es",suma)
+print("la resta de",numero1,"y",numero2,"es",resta)
+print("la multiplicación de",numero1,"y",numero2,"es",multiplicación)
+print("la división de",numero1,"y",numero2,"es",división)
